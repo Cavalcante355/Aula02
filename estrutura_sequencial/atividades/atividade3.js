ca70aeafdada166dbf808543e9588eab2dec019a
Cavalcante355 <samuelasaferodrigues@gmail.com>
2024-08-01 08:05:03 -0300

    Atualizando repositorio

--- estrutura_sequencial/atividades/atividade3.js
+++ estrutura_sequencial/atividades/atividade3.js
@@ -1,9 +1,14 @@
-let numero1, numero2, soma, subtrcao, mutiplicacao, divisao
+let numero1, numero2, soma, subtracao, mutiplicacao, divisao
 
 numero1 = parseInt(prompt("Digite um número:"));
 numero2 = parseInt(prompt("Digite um número:"));
 
-soma = `A soma desses números é: ${numero1 + numero2} <br>`
+soma = `<h1>A soma desses números é: ${numero1 + numero2}</h1> <br>`
 subtracao = `A subtracao dos números é: ${numero1 - numero2} <br>`
 mutiplicacao = `A mutiplicacao desses números deu: ${numero1 * numero2} <br>`
 divisao = `A divisao desses números deu: ${numero1 / numero2} <br>`
+
+document.write(soma)
+document.write(subtracao)
+document.write(mutiplicacao)
+document.write(divisao)
